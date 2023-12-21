b5c80a53328e2414193bf0170c80e24bd393b79d
unknown <eloissemolina@gmail.com>
2023-12-21 17:44:07 -0600

    añadiendo tabla

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,11 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { TablaComponent } from './components/tabla/tabla.component';
 import { FormComponent } from './components/form/form.component';
-
+import { TableComponent } from './components/table/table.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, TablaComponent, FormComponent],
+  imports: [CommonModule, RouterOutlet, TablaComponent, FormComponent, TableComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
